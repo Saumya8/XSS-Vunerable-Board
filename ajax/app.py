6eac3253656329a13f24484d2a1e4dd92ed9d6f2
Pranathi-29 <39451274+Pranathi-29@users.noreply.github.com>
2021-07-14 12:20:20 +0530

    Add files via upload

--- ajax/app.py
+++ ajax/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-
+    input_query=request.args.get('input')
+    return render_template('index.html', search_query=input_query)
     
- 
+
+
+
 
 if __name__ == "__main__":
     app.run()
