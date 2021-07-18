61638a9fee504f699f8a8d002e12438b9099e2a9
Hitesh <shettyhitesh10@gmail.com>
2021-07-19 04:02:04 +0530

    stored functionality added

--- ajax/app.py
+++ ajax/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request
+import db
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def index():
     input_query=request.args.get('input')
-    return render_template('index.html', search_query=input_query)
+    comments = db.get_comments(input_query)
+
+    if request.method == 'POST':
+        db.add_comment(input_query)
+
+
+
+    return render_template('index.html', 
+    comments = comments,
+    search_query=input_query)
     
 
 
