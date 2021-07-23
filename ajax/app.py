3c5acaa732c2864d7922ff027f0713ba852d13a5
Saumya8 <63854202+Saumya8@users.noreply.github.com>
2021-07-23 23:53:35 +0530

    Error: Don't commit yet

--- ajax/app.py
+++ ajax/app.py
@@ -101,7 +101,6 @@ def logout():
 def signup():
     print("start signup")
     if request.method == 'POST':
-        db.connect_db()
         #creating a default email for non-null
         email = "S"; password ="S"; print("input start")
         email = request.form['username']
@@ -137,7 +136,6 @@ def login():
         return redirect(url_for('search'))
 
     if request.method == 'POST':
-        db.connect_db()
         print("take input")
         email = request.form['username']
         password = request.form['password']
